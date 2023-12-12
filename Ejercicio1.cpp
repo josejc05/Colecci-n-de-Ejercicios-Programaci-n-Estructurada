25aa918bc65147fa809273769e2bdc5737dc1d49
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-12 11:53:22 +0100

    commit 2

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -6,3 +6,16 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    Estudiante *estudiante2 = new Estudiante;
+    strcpy(estudiante2->nombre, "Ana");
+    estudiante2->edad = 22;
+    estudiante2->promedio = 9.8;
+
+    Estudiante *punteroEst = &estudiante1;
+    printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", punteroEst->nombre, punteroEst->edad, punteroEst->promedio);
