f4a98f17fca5988133bff265a314ed20ab457328
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-12 12:31:04 +0100

    commit 2

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -19,3 +19,17 @@ void modificarEstudiantePorReferencia(Estudiante &est) {
 void imprimirEstudiantePorDireccion(Estudiante *est) {
     printf("Nombre: %s, Edad: %d, Promedio: %.2f\n", est->nombre, est->edad, est->promedio);
 }
+int main() {
+    Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    imprimirEstudiantePorValor(estudiante1);
+
+    modificarEstudiantePorReferencia(estudiante1);
+
+    imprimirEstudiantePorDireccion(&estudiante1);
+
+    return 0;
+}
