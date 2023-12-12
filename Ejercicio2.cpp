906e20f303b9f0a4a56eff18a6f045f914c669e5
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-12 12:04:48 +0100

    commit 2

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -6,3 +6,17 @@ union Dato {
     float f;
     char str[20];
 };
+int main() {
+    union Dato dato;
+
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
+
+    strcpy(dato.str, "C++ programming");
+    printf("dato.str: %s\n", dato.str);
+
+    return 0;
+}
